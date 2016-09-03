aacc9d2e7a9e3df0d620deeb733e838acc27b999
raman <buzaubakov.raman@gmail.com>
2016-09-03 11:29:47 +0600

    week2 upd

--- week2/main.cpp
+++ week2/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <string>
 
+/*
+@author: braman
+http://codeforces.com/problemset/problem/1/A
+*/
+
 // #define <name> <what you want>
 #define int64 unsigned long long int 
 
