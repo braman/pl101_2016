a377f78a132be2a7dc50a2b282da0e935ee8284f
raman <buzaubakov.raman@gmail.com>
2016-11-05 12:35:55 +0600

    week 11

--- week11/week11_struct.cpp
+++ week11/week11_struct.cpp
@@ -20,13 +20,74 @@ void prints(student s) {
     cout << " phone  - " << s.phone << endl;
 }
 
+int addStudent(vector<student> &v) {
+    string name;
+    int age;
+    bool gender;
+    int phone;
+
+    cout << "Enter name:";
+    cin >> name;
+
+    cout << "Enter age:";
+    cin >> age;
+
+    cout << "Enter gender:";
+    char g;
+    cin >> g;
+
+    gender = g == 'M';
+
+    cout << "Enter phone:";
+    cin >> phone;
+
+
+    student s = {ids++, age, gender, name, phone};
+
+    v.push_back(s);
+
+    cout << "new student was added!" << endl;
+}
+
+void displayMenu() {
+    cout << "Menu:" << endl;
+    cout << "1.Add Student" << endl;
+    cout << "2.Display students" << endl;
+    cout << "3.Remove students by id" << endl;
+    cout << "0.Exit" << endl;
+}
+
 int main() {
-
-    student s = {ids++, 25, true, "Test name", 707654622};
-
     vector<student> students;
 
-    prints(s);
+    int cmd = 0;
+
+
+    do {
+        displayMenu();
+
+        cin >> cmd;
+
+        switch(cmd) {
+            case 1:
+                addStudent(students);
+            break;
+            case 2:
+                //TODO: display all students
+            break;
+            case 3:
+                //TODO: remove student by id
+            break;
+            default:
+                cout << "unknow command" << endl;
+
+
+        }
+
+
+    } while (cmd > 0);
+
+    //prints(s);
 
     return 0;
 }
